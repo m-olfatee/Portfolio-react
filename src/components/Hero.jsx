443830063729ece6bd6c459ea9f1039cb4fed7f8
Mohammad Olfatee <m.olfatee@gmail.com>
2022-04-11 17:04:29 +0200

    Hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
                 <section className="hero-text">
                     <p>Hi, I'm</p>
                     <div className="name">
-                        <span className="letterAnimationTest1">M</span>
-                        <span className="letterAnimationTest2">O</span>
-                        <span className="letterAnimationTest3">H</span>
-                        <span className="letterAnimationTest4">A</span>
-                        <span className="letterAnimationTest5">M</span>
-                        <span className="letterAnimationTest3">M</span>
-                        <span className="letterAnimationTest4">A</span>
-                        <span className="letterAnimationTest5">D</span>
+                        <span>M</span>
+                        <span>O</span>
+                        <span>H</span>
+                        <span>A</span>
+                        <span>M</span>
+                        <span>M</span>
+                        <span>A</span>
+                        <span>D</span>
                     </div>
                     <p>Fullstack (MERN Stack) <br /> Web Developer </p>
                 </section>
